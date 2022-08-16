014bb82b3f51c84afa60cdaf9d8ab6c218a7dfc8
nHellstrom <nikhellstrom@hotmail.com>
2022-08-16 23:25:18 +0200

    Finished

--- PickRandomCard/Program.cs
+++ PickRandomCard/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PickRandomCard;
+
+Console.Write("How many cards shall be drawn? ");
+string drawAmount = Console.ReadLine();
+bool wroteNumber = false;
+
+Console.WriteLine();
+
+do
+{
+    if (int.TryParse(drawAmount, out int numberOfCards))
+    {
+        wroteNumber = true;
+        int draw = 1;
+        string[] drawnCards = CardPicker.PickSomeCards(numberOfCards);
+
+        foreach (var card in drawnCards)
+        {
+            if (draw % 2 == 0)
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Magenta;
+            }
+
+            Console.WriteLine(format: "Card #{0,2}: {1,18}",draw++, card);
+        }
+
+        Console.BackgroundColor = ConsoleColor.Black;
+
+    }
+    else
+    {
+        Console.WriteLine("Please repeat your number");
+    }
+}
+while (!wroteNumber);
+
+
